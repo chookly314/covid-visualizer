c3ac5837ff4085fbefabc605e003b5383ababc49
chookly <onomatopeyalamenor@gmail.com>
2020-04-06 14:29:47 +0200

    Initial Commit

---
+++ covid-visualizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-visualizer
+//
+//  Created by Fernando Cepeda on 06/04/2020.
+//  Copyright © 2020 fcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
